ce8d0f2aaaca4b15c628e12e6059a89bba120684
Rodolfo D. Arce S <info@rodolfoarce.com>
2023-05-06 16:49:37 -0400

    Primeras pruebas con autenticacion y obtener datos

---
+++ services/programmingLanguages.js
@@ -0,0 +1,22 @@
+const db = require('./db.js');
+const helper = require('../helper.js');
+const config = require('../config.js');
+
+async function getMultiple(page = 1){
+  const offset = helper.getOffset(page, config.listPerPage);
+  const rows = await db.query(
+    `SELECT id, name, released_year, githut_rank, pypl_rank, tiobe_rank 
+    FROM programming_languages LIMIT ${offset},${config.listPerPage}`
+  );
+  const data = helper.emptyOrRows(rows);
+  const meta = {page};
+
+  return {
+    data,
+    meta
+  }
+}
+
+module.exports = {
+  getMultiple
+}
