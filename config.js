b8e599f1166ba7c25956d29a6616468eb8e621fc
Rodolfo D. Arce S <info@rodolfoarce.com>
2023-05-06 16:50:14 -0400

    Configuracion de conexion

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+      /* don't expose password or any sensitive info, done only for demo */
+      host: "db4free.net",
+      user: "prueba_js",
+      password: "prueba_js.",
+      database: "prueba_js",
+    },
+    listPerPage: 10,
+  };
+  module.exports = config;
